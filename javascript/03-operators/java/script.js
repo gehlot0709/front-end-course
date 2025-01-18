76234ff0ea38dc50324d755d37d8a931f56858b2
gehlot0709 <pdgehlot956@gmail.com>
2025-01-18 15:49:10 +0530

    javascript

---
+++ javascript/03-operators/java/script.js
@@ -0,0 +1,83 @@
+/* ================================  operators  ============================================================*/
+
+
+/* ===================== Addition Operator ====================*/
+
+let maths = 5;
+let sci = 4;
+
+console.log(maths + sci)
+
+/*	===Subtraction===*/
+
+console.log(maths - sci)
+
+/* 	=== Multiplication ===*/
+
+console.log(maths * sci)
+
+/* === Exponentiation === */
+
+console.log(maths ** sci)
+
+/* === 	Division ===*/
+
+console.log(maths / sci)
+
+/* === 	Modulus (Remainder) === */
+
+console.log(maths % sci)
+
+/* === 	Increment === */
+
+console.log(maths++) // post increment
+
+/* === 	Decrement === */
+
+console.log(--sci) // pre increment
+
+
+/* ============ JavaScript Comparison Operators  =====================*/
+
+let a = 5;
+let b = "6";
+
+// equal to ====
+
+console.log( a == b)
+
+// equal value and equal type
+
+console.log( a === b)
+
+// 	not equal
+
+console.log( a != b)
+
+// not equal value or not equal type
+
+console.log( a !== b)
+
+// greater than
+
+console.log( a > b)
+
+// 	less than
+
+console.log( a < b)
+
+// greater than or equal to
+
+console.log( a >= b)
+
+// less than or equal to
+
+console.log ( a <= b)
+
+
+
+
+
+
+
+
