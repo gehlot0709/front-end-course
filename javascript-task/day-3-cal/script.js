52bd80781b7f4702300ea04193b9eaca9aae70aa
gehlot0709 <pdgehlot956@gmail.com>
2025-02-03 16:33:20 +0530

    add

--- javascript-task/day-3-cal/script.js
+++ javascript-task/day-3-cal/script.js
@@ -1,6 +1,6 @@
-let value1 = Number(prompt("enter your first value"))
-let operator = prompt("enter (*,+,-,/)")
-let value2 = Number(prompt("enter your value2"))
+// let value1 = Number(prompt("enter your first value"))
+// let operator = prompt("enter (*,+,-,/)")
+// let value2 = Number(prompt("enter your value2"))
 
 // if (operator === "+") {
 //     console.log(value1+value2);
@@ -12,20 +12,62 @@ let value2 = Number(prompt("enter your value2"))
 //     console.log(value1-value2);
 // }
 
-switch (operator) {
-    case "+":
-        console.log(value1+value2);
-        break;
-        case "-":
-            console.log(value1-value2);
-            break;
-            case "*":
-                console.log(value1*value2);
-                break;
-                case "/":
-                    console.log(value1/value2);
-                    break;
+// switch (operator) {
+//     case "+":
+//         console.log(value1+value2);
+//         break;
+//         case "-":
+//             console.log(value1-value2);
+//             break;
+//             case "*":
+//                 console.log(value1*value2);
+//                 break;
+//                 case "/":
+//                     console.log(value1/value2);
+//                     break;
 
-    default:
-        break;
+//     default:
+//         break;
+// }
+
+let num = Number(prompt("enter your number"))
+let output = false;
+
+if (num%2==0) {
+    output=true;
+    console.log("2");
 }
+if (num%3==0) {
+    output=true;
+    console.log("3");
+}
+if (num%4==0) {
+    output=true;
+    console.log("4");
+}
+if (num%5==0) {
+    output=true;
+    console.log("5");
+}
+if (num%6==0) {
+    output=true;
+    console.log("6");
+}
+if (num%7==0) {
+    output=true;
+    console.log("7");
+}
+if (num%8==0) {
+    output=true;
+    console.log("8");
+}
+if (num%9==0) {
+    output=true;
+    console.log("9");
+}
+if (!output) {
+    console.log("not valid");
+}
+
+
+
---
+++ javascript/07-array/script.js
@@ -0,0 +1,7 @@
+const ahir = [14,15,10,88,89,95]
+console.log(ahir);
+
+ahir[2]=15;
+console.log(ahir);
+
+console.log(ahir[3],ahir[5]);
