b882fa3197d417df7ca328bdbbb0747a92335f5d
gehlot0709 <pdgehlot956@gmail.com>
2025-03-26 16:07:54 +0530

    add

--- javascript/15-attribute/js/script.js
+++ javascript/15-attribute/js/script.js
@@ -1,11 +1,6 @@
 const update = document.querySelector("#update")
 const deletee = document.getElementById("delete")
-update.style.backgroundColor ="red"
-update.style.textDecoration ="none"
-update.style.padding = "8px 38px"
-deletee.style.padding = "8px 38px"
-deletee.style.backgroundColor ="orange"
-deletee.style.textDecoration ="none"
+
 
 console.log(update.attributes);
 console.log(update.getAttribute("href"));
